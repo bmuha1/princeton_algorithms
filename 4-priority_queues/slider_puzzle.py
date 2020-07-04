877bdd8b8260d9f68b71800ab80d129ee887385a
Brent Muha <brent.muha@gmail.com>
2020-07-03 17:42:35 -0700

    Add Manhattan distance

--- 4-priority_queues/slider_puzzle.py
+++ 4-priority_queues/slider_puzzle.py
@@ -39,11 +39,32 @@ class Board:
                 current += 1
         return tiles
 
+    def manhattan(self):
+        # Return the sum of the Manhattan distances (sum of the vertical and
+        # horizontal distance) between tiles and goal
+        goal, actual = {}, {}
+        current, y, x = 1, 0, 0
+        while current <= self.size ** 2:
+            goal[current] = [y, x]
+            actual[self.tiles[y][x]] = [y, x]
+            x += 1
+            if x == self.size:
+                y, x = y + 1, 0
+            current += 1
+
+        distance = 0
+        for i in range(1, self.size ** 2):
+            distance += abs(goal[i][0] - actual[i][0])
+            distance += abs(goal[i][1] - actual[i][1])
+        return distance
+
 
 if __name__ == '__main__':
     board = Board(3, [0, 1, 3, 4, 2, 5, 7, 8, 6])
     board2 = Board(3, [8, 1, 3, 4, 0, 2, 7, 6, 5])
     print(board)
     print('hamming:', board.hamming())
+    print('manhattan:', board.manhattan())
     print(board2)
     print('hamming:', board2.hamming())
+    print('manhattan:', board2.manhattan())
